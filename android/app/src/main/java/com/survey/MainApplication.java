475fc5bbf75da36516139769c13c77f3f761a6bb
Crystal Chen <hui_chen@brown.edu>
2018-04-20 16:11:02 -0400

    removed package to fix build error

--- android/app/src/main/java/com/survey/MainApplication.java
+++ android/app/src/main/java/com/survey/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.wix.reactnativenotifications.RNNotificationsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativePushNotificationPackage(),
-            new RNNotificationsPackage()
+            new ReactNativePushNotificationPackage()
       );
     }
 

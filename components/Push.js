10c25908bc80aae7ad3555cf2d8343dbdce880df
Crystal Chen <hui_chen@brown.edu>
2018-04-20 13:10:36 -0400

    sets up notifications package, will look into scheduling notifications

---
+++ components/Push.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import {
+  PushNotification
+} from 'react-native-push-notification';
+
+export default class Push extends Component<Props> {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    PushNotification.configure({
+        onNotification: function(notification) {
+            console.log('NOTIFICATION', notification);
+        },
+        popInitialNotification: true,
+    });
+  }
+  render() {
+    return null;
+  }
+}
+

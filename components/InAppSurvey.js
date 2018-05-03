cb0868e883800c9062973b9e7967f33c22037296
Emily Blatt <emilyblattds@gmail.com>
2018-05-03 02:11:42 -0400

    added qualtrics survey

--- components/InAppSurvey.js
+++ components/InAppSurvey.js
@@ -2,19 +2,46 @@ import React, { Component } from 'react';
 
 
 import { WebView } from 'react-native';
-import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+
 
 export default class InAppSurvey extends Component {
   constructor() {
     super();
+    // this.state = {
+    //    apiResponse: null,
+    //   currentLinkID: ''
+    // };
+    //  handleChangeNoteId = (event) => {
+    //     this.setState({currentLinkID: event});
   }
+}
+
+   // async saveNote() {
+   //    let newNote = {
+   //      body: {
+   //        "NoteTitle": "My first note!",
+   //        "NoteContent": "This is so cool!",
+   //        "NoteId": this.state.noteId
+   //      }
+   //    }
+   //    const path = "/Notes";
+
+   //    // Use the API module to save the note to the database
+   //    try {
+   //      const apiResponse = await API.put("NotesCRUD", path, newNote)
+   //      console.log("response from saving note: " + apiResponse);
+   //      this.setState({apiResponse});
+   //    } catch (e) {
+   //      console.log(e);
+   //    }
+   //  }
 
 
 
   render() {
     return (
       <WebView
-        source={{uri: 'https://google.com'}}
+        source={{uri: 'https://brown.co1.qualtrics.com/jfe/form/SV_cu9ppqo1mckUqSF'}}
         style={{height: '80%'}}
         domStorageEnabled={true}
         scalesPageToFit={true}              

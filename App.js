f6696e0595f9d46644d7895a5dbf1f7f75c582fa
josimwinga <josimwinga@gmail.com>
2018-05-06 18:04:03 -0400

    Revert "updated survey linke"
    This reverts commit 503ef0fc42e95ec0c54bc5f47b00c6a40cae3167.

--- App.js
+++ App.js
@@ -4,15 +4,9 @@ import Push from './components/Push.js';
 import PushNotification from 'react-native-push-notification';
 import {AppState, View, AsyncStorage} from 'react-native';
 import {scheduleNotification, setNotify} from 'survey/components/Helpers.js';
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
 import aws_exports from 'survey/aws-exports';
-=======
->>>>>>> c6b31edb9886e8c728e7bf61e9e53fdba4ded714
 import Amplify, { API } from 'aws-amplify';
->>>>>>> parent of 503ef0f... updated survey linke
 
 
 class App extends Component {
